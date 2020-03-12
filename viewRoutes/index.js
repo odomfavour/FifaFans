801ec3f6ad9c79051d835aa9ab9311ed95df78b3
Tueloper <ozurumbatochukwu@yahoo.com>
2020-03-12 22:00:43 +0100

    Testing files included and also favicon

--- viewRoutes/index.js
+++ viewRoutes/index.js
@@ -6,4 +6,9 @@ router.get('/', function (req, res, next){
 	res.render('index', { title: 'FIFA FANS' });
 });
 
+// this is the about page
+router.get('/about', function (req, res, next){
+	res.render('about', { title: 'FIFA FANS' });
+});
+
 module.exports = router;
