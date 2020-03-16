49282f1dc52fbdf1f10580b09f1f6e17055d66dd
Promise Orung <magnificentorung@gmail.com>
2020-03-16 02:30:09 +0100

    Ft-first commit

--- viewRoutes/index.js
+++ viewRoutes/index.js
@@ -11,4 +11,21 @@ router.get('/about', function (req, res, next){
 	res.render('about', { title: 'FIFA FANS' });
 });
 
+//this is the signup page
+
+router.get('/signup', function (req, res, next){
+	res.render('signup', { title: 'FIFA FANS' });
+});
+
+//this is login page
+router.get('/login', function (req, res, next){
+	res.render('login', { title: 'FIFA FANS' });
+});
+
+
+//this is login page
+router.get('/editprofile', function (req, res, next){
+	res.render('editprofile', { title: 'FIFA FANS' });
+});
+
 module.exports = router;
