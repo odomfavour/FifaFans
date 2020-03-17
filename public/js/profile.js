3cc1b95bbe2ffd2c8e829145730733dbc9a617b1
Promise Orung <magnificentorung@gmail.com>
2020-03-17 15:29:00 +0100

    making the active links and correcting the signup and login pages

---
+++ public/js/profile.js
@@ -0,0 +1,42 @@
+const edit_profile = document.getElementById('edit_profile');
+const change_password = document.getElementById('change_pass');
+const details_about_you = document.getElementById('details_abt_you');
+const contact_info = document.getElementById('contact_info');
+const title = document.getElementById('profile_page_title');
+
+let argUrl = window.location.pathname;
+console.log(argUrl);
+// if (this.readyState == 4 && this.status == 200) {
+//     header.innerHTML = this.responseText;
+// }
+switch (argUrl) {
+    case '/editprofile':
+    
+        document.querySelector('#edit_profile').classList.add('active');
+        break;
+    case '/changepassword':
+        console.log(argUrl);
+        document.querySelector('#change_pass').classList.add('active');
+        break;
+    case '/aboutuser':
+        document.querySelector('#details_abt_you').classList.add('active');
+        break;
+    case '/usercontactinfo':
+        document.querySelector('#contact_info').classList.add('active');
+        break;
+    default:
+        break;
+}
+
+title.innerText = localStorage.getItem('title');
+
+    function changeTitle(array_elements, element) {
+        array_elements.forEach(x => {
+            x.addEventListener('click', () => {
+                localStorage.setItem('title', x.innerText);
+            })
+        });
+    }
+    
+
+changeTitle([edit_profile, change_password, details_about_you, contact_info], title);
