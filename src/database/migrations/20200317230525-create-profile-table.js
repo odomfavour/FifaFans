76e86063e2c04c8cfa5ae688ebc88a4d77f1e63b
Lovisgod <olifedayo94@gmail.com>
2020-03-18 00:18:52 +0100

    Model setup for user details

---
+++ src/database/migrations/20200317230525-create-profile-table.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Profiles', {
+			uuid: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4
+      },
+      user_uuid: {
+				type: Sequelize.UUID
+			},
+      gender: Sequelize.STRING,
+      shortBio: Sequelize.STRING,
+      favoriteQuote: Sequelize.STRING,
+      language: Sequelize.STRING,
+      website: Sequelize.STRING,
+      profile_pic: {
+				type: Sequelize.STRING
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Profiles');
+	}
+};
---
+++ src/models/User.js
@@ -0,0 +1,65 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define(
+		'User',
+		{
+			uuid: {
+				type: DataTypes.UUID,
+				defaultValue: DataTypes.UUIDV4,
+				primaryKey: true
+			},
+			name: DataTypes.STRING,
+			email: {
+				type: DataTypes.STRING,
+				allowNull: {
+					args: false,
+					msg: 'Please enter your Email Address'
+				},
+				unique: {
+					args: true,
+					msg: 'Email Already Exist!!!'
+				},
+				validate: {
+					isEmail: {
+						args: true,
+						msg: 'Please Enter A Valid Email Address'
+					}
+				}
+			},
+			phone: DataTypes.REAL,
+			username: {
+				type: DataTypes.STRING,
+				allowNull: {
+					args: false,
+					msg: 'Please Enter Your Username'
+				},
+				unique: {
+					args: false,
+					msg: 'Username Already Exist'
+				}
+			},
+			password: DataTypes.STRING,
+			address: DataTypes.STRING,
+			verified: DataTypes.BOOLEAN,
+			role: {
+				type: DataTypes.ENUM('user', 'admin'),
+				defaultValue: 'user'
+			},
+			status: {
+				type: DataTypes.ENUM('coach', 'player', 'fan'),
+				defaultValue: 'inactive'
+            },
+            club: DataTypes.STRING,
+		},
+		{}
+	);
+	User.associate = function (models){
+		// associations can be defined here
+		User.hasMany(models.Token, {
+			foreignKey: 'user_uuid',
+			as: 'tokens',
+			onDelete: 'CASADE'
+		});
+	};
+	return User;
+};
