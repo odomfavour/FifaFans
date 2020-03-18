a7125c381c5933f30855b5a76d8285e506c450e1
Tueloper <ozurumbatochukwu@yahoo.com>
2020-03-18 19:46:57 +0100

    Auth codes setup

---
+++ src/routes/auth.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import AuthController from './../controllers/AuthController';
+import Auth from './../middleware/Auth';
+
+const userRouter = express.Router();
+
+const prfx = 'auth';
+
+userRouter.post(`/${prfx}/signup`, AuthController.signup);
+userRouter.post(`/${prfx}/signin`, AuthController.signin);
+userRouter.get(`/${prfx}/me`, Auth, AuthController.me);
+userRouter.get(`/${prfx}/verification/:token/:email/:id`, AuthController.verifyUser);
+userRouter.post(`/${prfx}/forgetpassword`, AuthController.forgetPassword);
+userRouter.get(`/${prfx}/verifypassword/:token/:email/:id`, AuthController.verifyPasswordLink);
+userRouter.post(`/${prfx}/resetpassword`, AuthController.resetPassword);
+userRouter.patch(`/${prfx}/updateprofile`, Auth, AuthController.updateUser);
+userRouter.post(`/${prfx}/refresh-email-token`, AuthController.getNewEmailToken);
+userRouter.get(`/${prfx}/usernames/email`, AuthController.getAllUserUsernameAndEmail);
+
+export default userRouter;
