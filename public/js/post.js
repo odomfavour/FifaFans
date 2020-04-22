8851eeddd4a237ea216f259b3a19eb6f139bfa90
Lovisgod <olifedayo94@gmail.com>
2020-04-22 15:07:36 +0100

    FT/room page

--- public/js/post.js
+++ public/js/post.js
@@ -102,7 +102,6 @@ async function listAllPosts() {
 function commentPost(post_uuid) {
     console.log(post_uuid)
     const post = document.getElementById(`${post_uuid}-comment-input`);
-    console.log(post)
     socketClient.emit('post-comment', { post_uuid, post: post.value })
     post.value = ''
   }
