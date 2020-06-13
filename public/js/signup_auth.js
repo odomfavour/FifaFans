4a872122d32f62887f69d55038742cbde33d5e49
Tueloper <ozurumbatochukwu@yahoo.com>
2020-06-13 15:33:17 +0100

    :sunny: Refactor in progress

--- public/js/signup_auth.js
+++ public/js/signup_auth.js
@@ -10,49 +10,56 @@ const confirmPassword = document.getElementById('confirmPassword');
 const checkbox = document.getElementById('checkbox');
 
 if (formList) {
-    formList.addEventListener('submit', onsubmit);
+	formList.addEventListener('submit', onsubmit);
 }
 
+function onsubmit (e){
+	e.preventDefault();
 
-function onsubmit(e) {
-    e.preventDefault();
-
-    if (!fullName.value || !userName.value || !email.value || !club.value || status.value === "" || password.value === "" || confirmPassword.value === "") {
-        errorMessage.innerHTML = 'Please enter all fields';
-        if (errorMessage.style.display == 'none') {
-            errorMessage.style.display = 'block'
-        }
-        setTimeout(function() {
-            errorMessage.style.display = 'none';
-        }, 10000);
-
-    } else if (password.value !== confirmPassword.value) {
-        document.getElementById('pass-message').innerHTML = 'Password doesn\'t match';
-        setTimeout(function() {
-            document.getElementById('pass-message').style.display = 'none';
-        }, 10000);
-
-    } else {
-        // this where the post to server will occur
-        const formData = new FormData();
-        formData.append('name', fullName.value);
-        formData.append('username', userName.value);
-        formData.append('email', email.value);
-        formData.append('password', password.value);
-        formData.append('role', 'user');
-        formData.append('club', club.value);
-        formData.append('status', status.value);
-        fetch('/api/v1/auth/signup', {
-                method: "POST",
-                body: formData,
-            })
-            .then(res => res.json())
-            .then(x => {
-                console.log(x);
-                if (x.status != 'error') {
-                    document.getElementById('success-message').innerHTML = x.data.message;
-                    return window.location.href = '/verify-message';
-                }
-            })
-    }
+	if (
+		!fullName.value ||
+		!userName.value ||
+		!email.value ||
+		!club.value ||
+		status.value === '' ||
+		password.value === '' ||
+		confirmPassword.value === ''
+	) {
+		errorMessage.innerHTML = 'Please enter all fields';
+		if (errorMessage.style.display == 'none') {
+			errorMessage.style.display = 'block';
+		}
+		setTimeout(function (){
+			errorMessage.style.display = 'none';
+		}, 3000);
+	}
+	else if (password.value !== confirmPassword.value) {
+		document.getElementById('pass-message').innerHTML = "Password doesn't match";
+		setTimeout(function (){
+			document.getElementById('pass-message').style.display = 'none';
+		}, 10000);
+	}
+	else {
+		// this where the post to server will occur
+		const formData = new FormData();
+		formData.append('name', fullName.value);
+		formData.append('username', userName.value);
+		formData.append('email', email.value);
+		formData.append('password', password.value);
+		formData.append('role', 'user');
+		formData.append('club', club.value);
+		formData.append('status', status.value);
+		fetch('/api/v1/auth/signup', {
+			method: 'POST',
+			body: formData
+		})
+			.then((res) => res.json())
+			.then((x) => {
+				console.log(x);
+				if (x.status != 'error') {
+					document.getElementById('success-message').innerHTML = x.data.message;
+					return (window.location.href = '/verify-message');
+				}
+			});
+	}
 }
