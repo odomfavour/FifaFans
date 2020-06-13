eb6b3bd7397c3e6fdbe5238567c563240d77e7a5
Tueloper <ozurumbatochukwu@yahoo.com>
2020-06-13 18:13:36 +0100

    :star: Sign bug fix in progress

--- public/js/signup_auth.js
+++ public/js/signup_auth.js
@@ -1,13 +1,12 @@
-const errorMessage = document.getElementById('error-message');
-const formList = document.getElementById('form-list');
-const fullName = document.getElementById('fullName');
-const userName = document.getElementById('username');
-const email = document.getElementById('email');
-const club = document.getElementById('club');
-const status = document.getElementById('status');
-const password = document.getElementById('password');
-const confirmPassword = document.getElementById('confirmPassword');
-const checkbox = document.getElementById('checkbox');
+const displayMessage = document.querySelector('#message');
+const formList = document.querySelector('#form-list');
+const fullName = document.querySelector('#fullName');
+const userName = document.querySelector('#username');
+const email = document.querySelector('#email');
+const status = document.querySelector('#status');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+const checkbox = document.querySelector('#checkbox');
 
 if (formList) {
 	formList.addEventListener('submit', onsubmit);
@@ -20,34 +19,22 @@ function onsubmit (e){
 		!fullName.value ||
 		!userName.value ||
 		!email.value ||
-		!club.value ||
 		status.value === '' ||
 		password.value === '' ||
 		confirmPassword.value === ''
 	) {
-		errorMessage.innerHTML = 'Please enter all fields';
-		if (errorMessage.style.display == 'none') {
-			errorMessage.style.display = 'block';
-		}
-		setTimeout(function (){
-			errorMessage.style.display = 'none';
-		}, 3000);
+		printMessage('Please Fill All Fields!!', 'alert-danger');
 	}
 	else if (password.value !== confirmPassword.value) {
-		document.getElementById('pass-message').innerHTML = "Password doesn't match";
-		setTimeout(function (){
-			document.getElementById('pass-message').style.display = 'none';
-		}, 10000);
+		printMessage('Passwords Does Not Match!!!', 'alert-danger');
 	}
 	else {
-		// this where the post to server will occur
 		const formData = new FormData();
 		formData.append('name', fullName.value);
 		formData.append('username', userName.value);
 		formData.append('email', email.value);
 		formData.append('password', password.value);
 		formData.append('role', 'user');
-		formData.append('club', club.value);
 		formData.append('status', status.value);
 		fetch('/api/v1/auth/signup', {
 			method: 'POST',
@@ -57,9 +44,20 @@ function onsubmit (e){
 			.then((x) => {
 				console.log(x);
 				if (x.status != 'error') {
-					document.getElementById('success-message').innerHTML = x.data.message;
+					document.querySelector('success-message').innerHTML = x.data.message;
 					return (window.location.href = '/verify-message');
 				}
 			});
 	}
 }
+
+const printMessage = (message, className) => {
+	const messageDiv = document.createElement('div');
+	messageDiv.classList.add('alert', 'infor', 'text-center', className);
+	messageDiv.setAttribute('role', 'alert');
+	messageDiv.appendChild(document.createTextNode(message));
+	displayMessage.innerHTML = messageDiv;
+	setTimeout(() => {
+		document.querySelector('.alert').remove();
+	}, 5000);
+};
