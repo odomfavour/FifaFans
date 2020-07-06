ccac933bfa11f88bcfa05d412f6164b288b887c3
Promise Orung <magnificentorung@gmail.com>
2020-07-06 11:08:12 +0100

    added the contact and about page

--- public/js/post.js
+++ public/js/post.js
@@ -278,7 +278,7 @@ const generalPost = (data) => {
                         <i class="fa fa-thumbs-up"></i> Like
                       </p>
                       <p class="p-2 text-right">
-                        <i class="fa fa-comments"></i> Comment
+                        <i class="fa fa-comments"></i> Comment <span class="comment-number">33</span>
                       </p>
                       <p class="p-2 text-right">
                         <i class="fa fa-share-alt"></i> Share
